31c2ca40354cd834b63468c93b1494b7c0e886db
AndroidMeda <hephappy@gmail.com>
2013-01-03 19:30:36 -0500

    Fix common include priority for p509
    PRODUCT_COPY_FILES: First definition wins if the file name is the same. Most specific first.
    DEVICE_PACKAGE_OVERLAYS: Last definition wins. Most specific last.
    CONFIG := xxx: Last definition wins. Most specific last.
    
    TESTED ON: configs/wpa_supplicant.conf
    
    Change-Id: I939ac9ff90a7ad607ea3836ca669c553ed9cdfab

--- p509.mk
+++ p509.mk
@@ -1,18 +1,12 @@
-PRODUCT_AAPT_CONFIG := normal mdpi
-PRODUCT_AAPT_PREF_CONFIG := mdpi
-
-$(call inherit-product, device/lge/msm7x27-common/device.mk)
-$(call inherit-product, vendor/lge/p509/p509-vendor.mk)
-$(call inherit-product, vendor/lge/msm7x27-common/msm7x27-common-vendor-blobs.mk)
-
-$(call inherit-product, device/mdpi-common/mdpi.mk)
-DEVICE_PACKAGE_OVERLAYS += device/lge/p509/overlay
+# Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # p509 configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thunder_keypad.kl:system/usr/keylayout/thunder_keypad.kl \
     $(LOCAL_PATH)/configs/thunder_keypad.kcm.bin:system/usr/keychars/thunder_keypad.kcm.bin \
-    $(LOCAL_PATH)/configs/touch_mcs6000.idc:system/usr/idc/touch_mcs6000.idc
+    $(LOCAL_PATH)/configs/touch_mcs6000.idc:system/usr/idc/touch_mcs6000.idc \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 
 # p509 init
 PRODUCT_COPY_FILES += \
@@ -29,17 +23,26 @@ PRODUCT_PACKAGES += \
     audio.primary.p509
 
 # Live wallpapers
-PRODUCT_COPY_FILES += packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 PRODUCT_PACKAGES += \
     LiveWallpapers \
     LiveWallpapersPicker \
     VisualizationWallpapers \
     librs_jni
 
-# Full-featured build of the Open-Source
-$(call inherit-product, build/target/product/full.mk)
+# Inherit products (Most specific first)
+$(call inherit-product, vendor/lge/p509/p509-vendor.mk)
+$(call inherit-product, device/lge/msm7x27-common/device.mk)
+$(call inherit-product, vendor/lge/msm7x27-common/msm7x27-common-vendor-blobs.mk)
 
+# Overrides
 PRODUCT_NAME := p509
 PRODUCT_DEVICE := p509
 PRODUCT_MODEL := LG-P509
+PRODUCT_MANUFACTURER := LGE
 
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+$(call inherit-product, device/mdpi-common/mdpi.mk)
+
+# P500 overlays (Most specific last)
+DEVICE_PACKAGE_OVERLAYS += device/lge/p509/overlay
