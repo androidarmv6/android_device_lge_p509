c50c826aea595102e23a770b69a76fb2955f1f86
AndroidMeda <hephappy@gmail.com>
2013-04-25 20:27:33 +0300

    Move libaudio to lge-common
    Change-Id: Ic3cfd43cee89eeed217260dbf936803abe650a05
    
    Conflicts:
    	BoardConfig.mk
    	p509.mk

--- p509.mk
+++ p509.mk
@@ -45,11 +45,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
 
-# p509 Audio
-PRODUCT_PACKAGES += \
-    audio_policy.p509 \
-    audio.primary.p509
-
 #P500_SPEAKER_IN_CALL_FIX
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
